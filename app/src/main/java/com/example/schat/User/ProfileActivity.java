faf8f1a2aaca5610b34e3217c30cd3533c73ea26
Amir Babaei <babaei.a94@gmail.com>
2019-06-05 17:42:49 -0700

    profile pic

--- app/src/main/java/com/example/schat/User/ProfileActivity.java
+++ app/src/main/java/com/example/schat/User/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.example.schat.User;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.schat.R;
@@ -25,6 +29,10 @@ public class ProfileActivity extends AppCompatActivity {
     TextView nameView, lastView, emailView;
     DatabaseReference userDb;
     String name, last, email;
+    ImageView profimg;
+    Uri ImageUri;
+
+    final static int gallPick =1;
 
 
     @Override
@@ -37,14 +45,40 @@ public class ProfileActivity extends AppCompatActivity {
         nameView = findViewById(R.id.name);
         lastView = findViewById(R.id.lastname);
         emailView = findViewById(R.id.email);
+        profimg =  findViewById(R.id.imageView);
         update = findViewById(R.id.update);
+
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 updateUserInfo();
             }
         });
+
+        profimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galerryIntent = new Intent();
+                galerryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galerryIntent.setType("image/*");
+                startActivityForResult(galerryIntent,gallPick);
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == gallPick && resultCode == RESULT_OK && data!=null)
+        {
+            ImageUri = data.getData();
+            profimg.setImageURI(ImageUri);
+        }
+
+    }
+
     void updateUserInfo()
     {
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -59,7 +93,10 @@ public class ProfileActivity extends AppCompatActivity {
             if(emailView.getText().toString() != null) {
                 userDb.child("user").child(user.getUid()).child("email").setValue(emailView.getText().toString());
             }
+
         }
         finish();
     }
+
+
 }
